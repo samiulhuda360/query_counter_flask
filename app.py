ebb53c3d2f7dd0622f3780f885262ae3184330c6
samiulhuda360 <samidurbar@gmail.com>
2024-05-09 19:34:17 +0600

    Login Pages

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def create_results_table():
 create_results_table()
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/gsc/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -59,13 +59,13 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/logout')
+@app.route('/gsc/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route('/')
+@app.route('/gsc/')
 def index():
     if current_user.is_authenticated:
         return render_template('index.html')
